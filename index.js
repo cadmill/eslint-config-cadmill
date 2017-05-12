8e9b1af09183eabf4547d3b396a7bbda06ddd7e8
Rudi Yardley <contact@rudiyardley.com>
2017-05-12 14:32:28 +0800

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    './eslint/best-practices.js',
+    './eslint/errors.js',
+    './eslint/es6.js',
+    './eslint/imports.js',
+    './eslint/node.js',
+    './eslint/react-a11y.js',
+    './eslint/react.js',
+    './eslint/style.js',
+    './eslint/variables.js',
+  ],
+  parser: 'babel-eslint'
+}
