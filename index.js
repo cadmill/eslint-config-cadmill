43effb068fa6648097116c939b2da66eb64a6087
Rudi Yardley <contact@rudiyardley.com>
2017-05-12 14:39:40 +0800

    Correct folder structure

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 module.exports = {
   extends: [
-    './eslint/best-practices.js',
-    './eslint/errors.js',
-    './eslint/es6.js',
-    './eslint/imports.js',
-    './eslint/node.js',
-    './eslint/react-a11y.js',
-    './eslint/react.js',
-    './eslint/style.js',
-    './eslint/variables.js',
+    './rules/best-practices.js',
+    './rules/errors.js',
+    './rules/es6.js',
+    './rules/imports.js',
+    './rules/node.js',
+    './rules/react-a11y.js',
+    './rules/react.js',
+    './rules/style.js',
+    './rules/variables.js',
   ],
   parser: 'babel-eslint'
 }
